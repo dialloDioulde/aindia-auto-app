3f8220917c2e743abc65ea6f071ed8b8ad470485
dialloDioulde <diouldmariia@hotmail.fr>
2023-11-01 02:14:23 -0400

    orders manage advanced

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -6,4 +6,12 @@
 
 class Constants {
   final GOOGLE_MAP_COUNTRIES = ["sn", "gm", "ci", "fr", "ca"];
+
+  // WEB SOCKETS EVENTS
+  final ROOM_CREATED = "roomCreated";
+  final ROOM_ALREADY_EXISTS = "roomAlreadyExists";
+  final LEFT_ROOM = "leftRoom";
+  final ROOM_NOT_FOUND = "roomNotFound";
+  final CLOSE = "close";
+  final ORDER_FROM_SERVER = "orderFromServer";
 }
